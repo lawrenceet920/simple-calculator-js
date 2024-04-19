333d08a27b3f783413c16b2a9898956bd4b721a5
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-04-19 13:56:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // Ethan Lawrence
 // April 19 2024
 // JS simple calculator
-document.getElementById(`btn`).addEventListener('click', start);
+document.getElementById('btn').addEventListener('click', start);
 
 function start() {
     // Imput
-    const fullName = `Ethan Lawrence`;
-    const date = `April 19 2024`;
-    const project = `JS simple calculator`;
+    const fullName = 'Ethan Lawrence';
+    const date = 'April 19 2024';
+    const project = 'JS simple calculator';
 
-    const num1 = prompt(`Enter Number 1`);
-    const num2 = prompt(`Enter Number 2`);
+    const num1 = prompt('Enter Number 1');
+    const num2 = prompt('Enter Number 2');
     // Process 
     // Using parse int prevents it from thinking it is a string, or from not entering a number
     const sol1 = parseInt(num1) + parseInt(num2);
@@ -31,10 +31,10 @@ function start() {
     console.log (`${num1} / ${num2} = ${sol4}`);
 
     // Creates output box
-    document.getElementById(`output1`).innerHTML = `${num1} + ${num2} = ${sol1}`;
-    document.getElementById(`output2`).innerHTML = `${num1} - ${num2} = ${sol2}`;
-    document.getElementById(`output3`).innerHTML = `${num1} * ${num2} = ${sol3}`;
-    document.getElementById(`output4`).innerHTML = `${num1} / ${num2} = ${sol4}`;
+    document.getElementById('output1').innerHTML = `${num1} + ${num2} = ${sol1}`;
+    document.getElementById('output2').innerHTML = `${num1} - ${num2} = ${sol2}`;
+    document.getElementById('output3').innerHTML = `${num1} * ${num2} = ${sol3}`;
+    document.getElementById('output4').innerHTML = `${num1} / ${num2} = ${sol4}`;
 
-    console.log (`**End Of Script**`);
+    console.log ('**End Of Script**');
 }
