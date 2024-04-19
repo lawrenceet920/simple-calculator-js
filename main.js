c7500aafc031b29696e7ef5b1fd0831c19b74989
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-04-19 12:49:09 -0400

    start

---
+++ main.js
@@ -0,0 +1,16 @@
+// Ethan Lawrence
+// April 19 2024
+// JS simple calculator
+
+// Imput
+const fullName = Ethan_Lawrence;
+const date = April_19_2024;
+const project = JS_simple_calculator;
+// Process 
+
+
+// Output
+
+console.log(fullName)
+console.log(date)
+console.log(project)
